2eb92bfb9445cd2a6a930131489381632f0dbd9c
rozzzly <pclienau@gmail.com>
2020-10-31 18:39:54 -0600

    add basic ScopeNode support for sliceing and branching

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,6 +60,7 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off'
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off'
     }
 };
