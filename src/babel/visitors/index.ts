2ad1c8608ae2267e36167828702804262eb8e79c
rozzzly <pclienau@gmail.com>
2020-12-20 18:23:28 -0600

    add support for Import  statements

--- src/babel/visitors/index.ts
+++ src/babel/visitors/index.ts
@@ -2,11 +2,15 @@ import { TraverseOptions } from '@babel/traverse';
 import BabelSource from '../BabelSource';
 import primitiveVisitors from './primitives';
 import operatorVisitors from './operators';
+import miscVisitors from './misc';
+import identifierVisitors from './identifiers';
 
 export function createVisitors(this: BabelSource): TraverseOptions {
     return [
         primitiveVisitors,
-        operatorVisitors
+        operatorVisitors,
+        identifierVisitors,
+        miscVisitors
     ].reduce((reduction, visitor) => ({
         ...reduction,
         ...(visitor.call(this)),
