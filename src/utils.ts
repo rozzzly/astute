40d691ba35a958cd7ae05b42cd4ae13d840c26dc
rozzzly <pclienau@gmail.com>
2021-01-28 22:10:13 -0600

    ScopeNode.search(...) wrapper for ScopeNode.walk(...) for quicker filtering

--- src/utils.ts
+++ src/utils.ts
@@ -37,3 +37,26 @@ export const castAsRanged: <R>(value: unknown) => asserts value is (
     & Omit<R, 'start' | 'end'>
     & Ranged
 ) = (value) => {};
+
+
+/**
+ * Runtime check/typeguard to determine if value is a RegExp
+ */
+export const isRegExp = (value: unknown): value is RegExp => (
+    Object.prototype.toString.call(value) === '[object RegExp]'
+);
+
+
+/**
+ * Require `T` to have at least one of the properties specified by the union `Keys`
+ * @see https://stackoverflow.com/questions/40510611/typescript-interface-require-one-of-two-properties-to-exist/49725198#49725198
+ */
+export type RequireSome<T, Keys extends keyof T = keyof T> = (
+    & Pick<T, Exclude<keyof T, Keys>>
+    & ({
+        [K in Keys]-?: (
+            & Required<Pick<T, K>>
+            & Partial<Record<Exclude<Keys, K>, undefined>>
+        );
+    })[Keys]
+);
